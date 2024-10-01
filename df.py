55979656a36ae227a0e5bbc4dd6a6d5516943d87
Samuel Oluleye <seun4unoted@gmail.com>
2024-10-01 23:44:36 +1000

    Update df.py

--- df.py
+++ df.py
@@ -1,4 +1,5 @@
 import ast
 import pandas as pd
 
-df = pd.read_csv("/home/sam/Neural networks/Telegram bots/.venv/Man United telegram bot with aiogram3/Man United.csv")
+embeddings_path = "https://storage.yandexcloud.net/man-united/Man%20United.csv"
+df = pd.read_csv(embeddings_path)
