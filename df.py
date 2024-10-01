e1529758fda60423e8c03c12b265ed93dbc9f01b
Samuel Oluleye <seun4unoted@gmail.com>
2024-10-01 23:53:40 +1000

    Update df.py

--- df.py
+++ df.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 embeddings_path = "https://storage.yandexcloud.net/man-united/Man%20United.csv"
 df = pd.read_csv(embeddings_path)
+# Convert our embeddings from strings to lists
+df['embedding'] = df['embedding'].apply(ast.literal_eval)
